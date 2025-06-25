64806fe51f897343e1d8d59fd70610c2629f2a4c
hsu-sin <shwesinhsumyat9@gmail.com>
2025-06-26 02:17:33 +0700

    finished

--- cell05/ex11/count_it.py
+++ cell05/ex11/count_it.py
@@ -5,6 +5,6 @@ import sys
 if len(sys.argv) == 1:
     print("none")
 else:
-    print(f"parameters: {len(sys.argv[1])-1}")
+    print(f"parameters: {len(sys.argv[1:])}")
     for i in sys.argv[1:]:
         print(f"{i} : {len(i)}")
